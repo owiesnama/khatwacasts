32c96adafde6723231f0ed20f5748eef35801e14
Ahmed Altaif <ahmedaltaif79@gmail.com>
2019-05-19 11:42:37 +0200

    initial commit

---
+++ database/migrations/2019_05_19_082602_create_podcasts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('url');
+            $table->text('title');
+            $table->text('description');
+            $table->double('length');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcasts');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+
+
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="card white">
+                    <div class="card-content">
+                        <span class="card-title">Podcast title</span>
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">
+                            <span  class="center-align valign-wrapper">
+                            <i class="material-icons">play_arrow</i>
+                            Play
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span class="center-align">
+                            <i class="material-icons">share</i>
+                            Share
+                            </span>
+
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+@endsection
